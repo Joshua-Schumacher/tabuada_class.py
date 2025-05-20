a2b0fb06c88db636d651ab2a93d3b23c0e4a8138
Joshua-Schumacher <joshuaschumacher2003@gmail.com>
2025-05-19 21:32:03 -0300

    Add files via upload

---
+++ tabuada_poo/classes.py
@@ -0,0 +1,27 @@
+class Tabuada:
+    def __init__(self,numero):
+        self.valor = numero
+        self._resultado = 0 
+
+    def soma(self):
+            for i in range(1,10+1):
+                self._resultado = self.valor + i
+                print(f"{self.valor} + {i} = {self._resultado}")
+
+    def subtracao(self):       
+            for i in range(1,10+1):
+                self._resultado = self.valor - i
+                print(f"{self.valor} - {i} = {self._resultado}")
+
+    def multiplicacao(self):
+         for i in range(1,10+1):
+              self._resultado = self.valor * i
+              print(f"{self.valor} * {i} = {self._resultado}")
+    
+    def divisão(self):
+         for i in range(1,10+1):
+              self._resultado = self.valor / i
+              print(f"{self.valor} / {i} = {self._resultado}")
+
+
+tabuada = Tabuada(10)
