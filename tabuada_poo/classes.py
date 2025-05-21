63cb3e59ea426dbda2da21d017b9605dcb4baf2f
Joshua-Schumacher <joshuaschumacher2003@gmail.com>
2025-05-21 18:53:26 -0300

    separar arquivos

--- tabuada_poo/classes.py
+++ tabuada_poo/classes.py
@@ -24,12 +24,3 @@ class Tabuada:
               print(f"{self.valor} / {i} = {self._resultado}")
 
 
-tabuada_2 = Tabuada(2)
-
-tabuada_2.soma()
-print("---"*15)
-tabuada_2.subtracao()
-print("---"*15)
-tabuada_2.multiplicacao()
-print("---"*15)
-tabuada_2.divisão()
---
+++ tabuada_poo/main.py
@@ -0,0 +1,11 @@
+from classes import Tabuada
+
+tabuada_2 = Tabuada(2)
+
+tabuada_2.soma()
+print("---"*15)
+tabuada_2.subtracao()
+print("---"*15)
+tabuada_2.multiplicacao()
+print("---"*15)
+tabuada_2.divisão()
